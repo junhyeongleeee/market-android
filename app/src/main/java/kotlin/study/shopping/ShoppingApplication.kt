b4579a3451bd5e910eab1512dd80436f3981dce7
junhyeongleee <djskal3745@skuniv.ac.kr>
2022-05-15 02:48:25 +0900

    koin structure create
    yet add modules

---
+++ app/src/main/java/kotlin/study/shopping/ShoppingApplication.kt
@@ -0,0 +1,20 @@
+package kotlin.study.shopping
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class ShoppingApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@ShoppingApplication)
+            modules(listOf())
+        }
+    }
+}
